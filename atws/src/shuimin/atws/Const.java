88c507f37f2bb691cb8483ad84bb5fa6e8eeb9e5
edwinyxc@gmail.com <edwinyxc@gmail.com>
2013-12-12 12:17:13 +0800

    add content

---
+++ atws/src/shuimin/atws/Const.java
@@ -0,0 +1,31 @@
+package shuimin.atws;
+
+
+/**
+ * place for constants
+ * @author ed
+ * 
+ */
+public final class Const
+{
+	final static public class APP
+	{
+		final public static String PKG_NAME = "shuimin.atws";
+	}
+
+	final static public class MODULE
+	{
+		final public static String PROP_KEY_PKG_NAME = "module.pkg_name";
+	}
+
+	final static public class RESOURCE
+	{
+//		final public static String SUFFIXES = "jsp,xhtml,tpl,xml,js,html,css,jpg,png,jpeg,gif,font,dict,db";
+	}
+
+//	public static String key(Field f)
+//	{
+//		final String ret = ("" + f.getClass().getCanonicalName() + f.getName());
+//		return (ret.length() > APP.PKG_NAME.length() + 1) ? ret.substring(APP.PKG_NAME.length() + 1) : null;
+//	}
+}
